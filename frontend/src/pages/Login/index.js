3606c1c42cf7d03af982f2af8c0448251cb17dfe
João Lima <jpalvesl101@gmail.com>
2020-02-16 19:51:59 -0300

    adicionando formulario de login

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 function Login() {
   return (
-    <h1>Futuro Login</h1>
+    <form>
+    <div class="form-group">
+      <label for="user">Digite seu user</label>
+      <input type="text" class="form-control" id="user" placeholder="Seu user" />
+    </div>
+    <div class="form-group">
+      <label for="exampleInputPassword1">Senha</label>
+      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha" />
+    </div>
+      <button type="submit" class="btn btn-primary">Enviar</button>
+    </form>
   )
 }
 
