6bb4f602e2d9c64367a844f7098029cab8a16d62
Heitor <he1tor@hotmail.com>
2020-02-17 22:38:19 -0300

    :heavy_plus_sign: @material-ui/core and @material-ui/icons :poop:

--- frontend/src/pages/Game/index.js
+++ frontend/src/pages/Game/index.js
@@ -1,21 +1,79 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Collapse from '@material-ui/core/Collapse';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
 
-import './styles.css';
 
-/**
-import coin from '../../../assets/images/coin.png';
-import vaso from '../../../assets/images/vaso.png';
-import usuarioCel from '../../../assets/images/usuarioCel.jpg';
-import usuarioCelDois from '../../../assets/images/usuarioCelDois.jpg';
-*/
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+  nested: {
+    paddingLeft: theme.spacing(4),
+  },
+}));
 
-function Game() {
+export default function Game() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(true);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
   return (
-    <>
-    teste
-    </>
-  )
+    <List
+      component="nav"
+      aria-labelledby="nested-list-subheader"
+      subheader={
+        <ListSubheader component="div" id="nested-list-subheader">
+          Game
+        </ListSubheader>
+      }
+      className={classes.root}
+    >
+      <ListItem button onClick={handleClick}>
+        <ListItemText primary="Fase 1" />
+        {open ? <ExpandLess /> : <ExpandMore />}
+      </ListItem>
+      <Collapse in={open} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          <ListItem button className={classes.nested}>
+            <ListItemIcon>
+            </ListItemIcon>
+            <ListItemText primary="Renda fixa" />
+          </ListItem>
+          <ListItem button className={classes.nested}>
+            <ListItemIcon>
+            </ListItemIcon>
+            <ListItemText primary="Renda Variavel" />
+          </ListItem>
+          <ListItem button className={classes.nested}>
+            <ListItemIcon>
+            </ListItemIcon>
+            <ListItemText primary="Comparação" />
+          </ListItem>
+          <ListItem button className={classes.nested}>
+            <ListItemIcon>
+            </ListItemIcon>
+            <ListItemText primary="Desafio" />
+          </ListItem>
+        </List>
+      </Collapse>
+      <ListItem button>
+        <ListItemText primary="Fase 2" />
+      </ListItem>
+      <ListItem button>
+        <ListItemText primary="Fase 3" />
+      </ListItem>
+    </List>
+  );
 }
-          
-export default Game;
